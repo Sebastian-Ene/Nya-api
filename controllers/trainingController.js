fe7b7445a93f1983fcedd313317512e848999221
Sebastian-Antonio ENE (100744) <sebastian.ene0202@stud.etti.upb.ro>
2020-11-15 16:02:04 +0200

    training model and controller adjustments

--- controllers/trainingController.js
+++ controllers/trainingController.js
@@ -3,14 +3,14 @@ const Training = require('../models/trainingModel');
 exports.getAllTrainings = async (req, res) => {
   try {
     const trainings = await Training.find();
-    return res.status(200).json({
+    res.status(200).json({
       status: 'succes',
       data: {
         trainings,
       },
     });
   } catch (err) {
-    return res.status(404).json({
+    res.status(404).json({
       status: 'fail',
     });
   }
@@ -20,32 +20,32 @@ exports.getTraining = async (req, res) => {
   try {
     const training = await Training.findById(req.params.id);
     if (!training) throw Error('No training at this id');
-    return res.status(200).json({
+    res.status(200).json({
       status: 'succes',
       data: {
         training,
       },
     });
   } catch (err) {
-    return await res.status(404).json({
+    await res.status(404).json({
       status: 'fail',
     });
   }
 };
 
 exports.createTraining = async (req, res) => {
-  const newTraining = new Training(req.body);
   try {
-    await newTraining.save();
-    return res.status(200).json({
+    const newTraining = await Training.create(req.body);
+    res.status(201).json({
       status: 'succes',
       data: {
         newTraining,
       },
     });
   } catch (err) {
-    return res.status(404).json({
+    res.status(404).json({
       status: 'fail',
+      error: err,
     });
   }
 };
